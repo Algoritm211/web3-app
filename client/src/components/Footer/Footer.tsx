6eb084268831c452ab5e3351d79907af1893970c
Alexey <algoritm211@gmail.com>
2022-05-07 23:32:27 +0300

    Add footer layout

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import logo from '../../../images/logo.png'
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      Footer
+    <div className="flex md:justify-center justify-between items-center
+    flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between
+      items-center my-4">
+        <div className="flex flex-1 justify-center items-center">
+          <img src={logo} alt="footer_img" className="w-32" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap
+        sm:mt-0 mt-5 w-full ">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Market
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Exchange
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Tutorials
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Wallets
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">
+          Join us
+        </p>
+        <p className="text-white text-sm text-center">
+          algoritm211@gmail.com
+        </p>
+      </div>
+      <div style={{height: '0.1px'}} className="w-full bg-gray-400 mt-5" />
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">
+          Alexey Horbunov, {new Date().getFullYear()}
+        </p>
+        <p className="text-white text-sm text-center">
+          All rights reserved
+        </p>
+      </div>
     </div>
   );
 };
